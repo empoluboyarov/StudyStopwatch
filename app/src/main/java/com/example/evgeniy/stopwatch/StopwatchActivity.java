dee2c71d458a3330656d8f2dc7b20c5cb4779bc3
empoluboyarov <poluboyarovem@gmail.com>
2016-04-02 19:37:12 +0300

    Layout

---
+++ app/src/main/java/com/example/evgeniy/stopwatch/StopwatchActivity.java
@@ -0,0 +1,26 @@
+package com.example.evgeniy.stopwatch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class StopwatchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stopwatch);
+    }
+
+    public void onClickStart(View view){
+
+    }
+
+    public void onClickStop(View view){
+
+    }
+
+    public void onClickReset(View view){
+
+    }
+}
